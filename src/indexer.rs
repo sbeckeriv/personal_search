9976e9d39605172cb09c5f249be2bb40d1aa24d1
becker <github@deathbyescalator.com>
2020-10-14 21:51:37 -0700

    header user agent

--- src/indexer.rs
+++ src/indexer.rs
@@ -31,7 +31,15 @@ pub enum GetterResults {
 pub trait IndexGetter {
     fn get_url(&self, url: &str) -> GetterResults {
         let agent = ureq::Agent::default().build();
-        let res = agent.get(url).timeout(Duration::new(10, 0)).call();
+        let res = agent
+            .get(url)
+            .set(
+                "User-Agent",
+                "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+            )
+            .set("X-Source", "https://github.com/sbeckeriv/personal_search")
+            .timeout(Duration::new(10, 0))
+            .call();
         dbg!(&res);
         if res.status() < 300 {
             if let Some(lower) = res.header("Content-Type") {
